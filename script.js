e0f8f859241dd78522213e97d9c0f9167fede115
xDGxx <cntt.dg@gmail.com>
2025-05-16 03:05:33 -0300

    Removendo bug do menu hamburguer

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Inicializa o AOS
   AOS.init();
-});
-const menuToggle = document.getElementById('menu-toggle');
-const menuNav = document.getElementById('menu-nav');
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  menuNav.classList.toggle('active');
-});
+  // Seleciona os elementos do menu
+  const menuToggle = document.getElementById('menu-toggle');
+  const menuNav = document.getElementById('menu-nav');
 
+  // Verifica se os elementos existem antes de adicionar o evento
+  if (menuToggle && menuNav) {
+    menuToggle.addEventListener('click', () => {
+      menuToggle.classList.toggle('active');
+      menuNav.classList.toggle('active');
+    });
+  } else {
+    console.error('Elementos menu-toggle ou menu-nav não encontrados.');
+  }
+});
